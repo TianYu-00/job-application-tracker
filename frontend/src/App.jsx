959ef2137545a33e4189eadfc232605ff80a99b8
Tian <66271788+TianYu-00@users.noreply.github.com>
2026-06-04 08:40:07 +0100

    spread out the navigation links inside header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
 import "./App.css";
-import Header from "./pages/Header";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Applications from "./pages/Applications";
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router";
 
 function Header() {
   return (
-    <header>
-      <nav>
+    <header className="px-6 py-4 border-b">
+      <nav className="flex gap-6">
         <Link to="/">Home</Link>
         <Link to="/applications">Applications</Link>
       </nav>
